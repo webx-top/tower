e72bf5a48536be8d4a7362caf52369e5094cfdb3
swh <swhbox@foxmail.com>
2016-07-04 15:43:37 +0800

    update

--- page.go
+++ page.go
@@ -74,7 +74,7 @@ func RenderAppError(ctx reverseproxy.Context, app *App, errMessage string) {
 
 func renderPage(ctx reverseproxy.Context, info ErrorInfo) {
 	ctx.SetHeader(`Content-Type`, `text/html;charset=utf-8`)
-	err := errorTemplate.Execute(ctx.RespWriter(), info)
+	err := errorTemplate.Execute(ctx.ResponseWriter(), info)
 	if err != nil {
 		panic(err)
 	}

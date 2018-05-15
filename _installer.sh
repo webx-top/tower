666cbe82e238a8ad8ba198302dcaf78de9c693e0
HankShen <swhbox@foxmail.com>
2018-05-15 16:38:27 +0800

    update

--- _installer.sh
+++ _installer.sh
@@ -1,3 +1,12 @@
-go get github.com/admpub/glide
-glide install
-go install
+# 安装引用包
+go get github.com/golang/sys
+mkdir $GOPATH/src/golang.org/x/sys
+mv $GOPATH/src/github.com/golang/sys/* $GOPATH/src/golang.org/x/sys
+sudo mv $GOPATH/src/github.com/golang/sys/.git* $GOPATH/src/golang.org/x/sys
+rm -rf $GOPATH/src/github.com/golang/sys
+
+go get github.com/golang/crypto
+mkdir $GOPATH/src/golang.org/x/crypto
+mv $GOPATH/src/github.com/golang/crypto/* $GOPATH/src/golang.org/x/crypto
+mv $GOPATH/src/github.com/golang/crypto/.git* $GOPATH/src/golang.org/x/crypto
+rm -rf $GOPATH/src/github.com/golang/crypto

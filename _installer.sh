9d78396ab636256c5580eae95c07d20695aa0ed0
Wenhui Shen <swhbox@foxmail.com>
2018-06-10 19:59:20 +0800

    update

--- _installer.sh
+++ _installer.sh
@@ -1,12 +1,7 @@
+mkdir -p $GOPATH/src/golang.org/x
 # 安装引用包
 go get github.com/golang/sys
-mkdir $GOPATH/src/golang.org/x/sys
-mv $GOPATH/src/github.com/golang/sys/* $GOPATH/src/golang.org/x/sys
-sudo mv $GOPATH/src/github.com/golang/sys/.git* $GOPATH/src/golang.org/x/sys
-rm -rf $GOPATH/src/github.com/golang/sys
+mv $GOPATH/src/github.com/golang/sys $GOPATH/src/golang.org/x/sys
 
 go get github.com/golang/crypto
-mkdir $GOPATH/src/golang.org/x/crypto
-mv $GOPATH/src/github.com/golang/crypto/* $GOPATH/src/golang.org/x/crypto
-mv $GOPATH/src/github.com/golang/crypto/.git* $GOPATH/src/golang.org/x/crypto
-rm -rf $GOPATH/src/github.com/golang/crypto
+mv $GOPATH/src/github.com/golang/crypto $GOPATH/src/golang.org/x/crypto

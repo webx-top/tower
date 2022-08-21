64d7cc61a7a64fba1b2d0659c6de4f98697a61e9
Hank Shen <swh@admpub.com>
2022-08-21 21:40:34 +0800

    update

--- pprof.go
+++ pprof.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	_ "net/http/pprof"
+
+	"github.com/admpub/log"
 )
 
 func startPprof(port int) {
 	addr := fmt.Sprintf("127.0.0.1:%d", port)
-	log.Printf("== Debug server URL: http://%s/debug/pprof/\n", addr)
-	log.Println(http.ListenAndServe(addr, nil))
+	log.Infof("== Debug server URL: http://%s/debug/pprof/", addr)
+	log.Error(http.ListenAndServe(addr, nil))
 }
